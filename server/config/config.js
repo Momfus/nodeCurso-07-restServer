82ccd5d86cb3317b2640083a76f5018ac6b0cf4f
Momfus <momfus@outlook.com>
2019-12-04 22:10:35 -0300

    Agregado configuraciones de uso de mongodb remoto con mongo atlas

--- server/config/config.js
+++ server/config/config.js
@@ -9,3 +9,25 @@
 // ========================================
 process.env.PORT = process.env.PORT || 3000; // Variable global que defino y toma el puerto que esta desplegable según heroku que lo actualiza por nosotros, sino localmente será 3000 (para desarrollo)
 
+
+// ========================================
+// Entorno
+// ========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; // Si la base de datos no existe, suponemos que estamos en desarrollo
+
+// ========================================
+// Base de datos
+// ========================================
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev' ) {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+
+    urlDB = 'mongodb+srv://test:NODxG6aKAjGxRlRm@cluster0-klubu.mongodb.net/cafe';
+
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const app = express();
 const bodyParser = require('body-parser'); // para manejar los cuerpos de servicios REST (middlewares, cada petición pasa por las dos lineas de abajo)
  
 
+/* 
+    MongoDb URL 
+    - Usando mongoDb atlas se puede generar un usuario, en mi ejemplo: " user: test; pass: NODxG6aKAjGxRlRm" (ver video 106 que se explica)
+    - cluster0-klubu.mongodb.net/   (explicado en video 107)
+*/
+
 //#region Configuraciones del parser middleware
 
 // Cada petición que se haga por express pasa por esas lineas de bodyParser)
@@ -23,7 +29,7 @@ app.use(bodyParser.json());
 app.use( require('./routes/usuario') ); // importamos y usamos las rutas del usuario en app (luego de la configuración de los middleware)
 
 // Abrir conexión con la BD
-moongoose.connect( 'mongodb://localhost:27017/cafe', //conexión y su url a la base de datos
+moongoose.connect( 'mongodb+srv://test:NODxG6aKAjGxRlRm@cluster0-klubu.mongodb.net/cafe', //'mongodb://localhost:27017/cafe', //conexión y su url a la base de datos (la segunda comentada es si fuera local)
                   {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true}, // configuraciones extras sobre una actualizacion de mongo 
                   (err, res) =>{  // callback por si hay una conexión válida o no
 
